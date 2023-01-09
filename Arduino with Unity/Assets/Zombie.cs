86f4b0a842f87d14542521787db2b426ba01e869
Mathias <maths20@student.sdu.dk>
2023-01-09 05:59:53 +0100

    v1 of idle / attack animation implementation
    - Pretty scuffed atm

--- Arduino with Unity/Assets/Zombie.cs
+++ Arduino with Unity/Assets/Zombie.cs
@@ -9,10 +9,12 @@ public class Zombie : MonoBehaviour
     float _speed = 1.0f;
     const float EPSILON = 0.1f;
     public TextMeshProUGUI debugText;
+
+    private Animation anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        anim = gameObject.GetComponent<Animation>();
     }
 
     // Update is called once per frame
@@ -25,5 +27,20 @@ public class Zombie : MonoBehaviour
 
         if((transform.position - _followTarget.position).magnitude > EPSILON)
             transform.Translate(0.0f, 0.0f, _speed * Time.deltaTime);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player"){
+            this.anim.Play("Zombie");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.tag == "Player"){
+            this.anim.Play("Zombie");
+        }
     }
 }
