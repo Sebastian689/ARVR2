0908023207bc8d4e98e3388fd874c209ede32651
Simon <mukopper@gmail.com>
2023-01-09 11:44:38 +0100

    Sample scene with working communication

---
+++ Arduino with Unity/Assets/MessageListener.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MessageListener : MonoBehaviour
+{
+    // Invoked when a line of data is received from the serial device.
+    void OnMessageArrived(string msg)
+    {
+        if(msg=="1"){
+            Debug.Log("Input");
+        }
+    }
+
+    // Invoked when a connect/disconnect event occurs. The parameter 'success'
+    // will be 'true' upon connection, and 'false' upon disconnection or
+    // failure to connect.
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log("Connected");
+    }
+}
